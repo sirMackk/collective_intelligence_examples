b945f57aebc45c5ddcd382c179b92fc16230f2ce
sirMackk <c.tairix@gmail.com>
2014-04-05 21:13:20 +0100

    Annotates searchengine.py for chapter 4

--- chap4/searchengine.py
+++ chap4/searchengine.py
@@ -22,6 +22,11 @@ class Crawler(object):
         self.con.commit()
 
     def get_entry_id(self, table, field, value, create_new=True):
+        '''
+        This method attempts to retrieve the primary key rowid of an
+        entry in 'table' where 'field' equals 'value'.
+        If it cannot find this entry, it creates it and returns the rowid.
+        '''
         cur = self.con.execute('select rowid from %s where %s=\'%s\'' %
                 (table, field, value))
         result = cur.fetchone()
@@ -33,6 +38,10 @@ class Crawler(object):
             return result[0]
 
     def add_to_index(self, url, soup):
+        '''
+        Registers the url in the url_list, then registers every word
+        (using soup) with it's url, word_id and location in the page.
+        '''
         if self.is_indexed(url):
             return
 
@@ -41,13 +50,18 @@ class Crawler(object):
         text = self.get_text_only(soup)
         words = self.separate_words(text)
 
+        # retrieves or sets and retrieves a url's rowid
         url_id = self.get_entry_id('url_list', 'url', url)
 
+        # iterates through all the words under a given url
         for i in xrange(len(words)):
             word = words[i]
             if word in ignore_words:
                 continue
+            # retrieves or sets and retrieves a word's rowid
             word_id = self.get_entry_id('word_list', 'word', word)
+            # record the url and word (by id) and location (by index of the
+            # in all the words on a page)
             self.con.execute('insert into word_location(url_id, word_id, location) \
                     values (%d, %d, %d)' % (url_id, word_id, i))
 
@@ -65,10 +79,20 @@ class Crawler(object):
         return None
 
     def separate_words(self, text):
+        '''
+        This function accepts a string and returns a list of lower-case
+        words.
+        '''
         splitter = re.compile('\\W*')
         return [s.lower() for s in splitter.split(text) if s != '']
 
     def is_indexed(self, url):
+        '''
+        This function checks if a url has been entered into the url_list
+        table and if yes, then further checks if an entry has been made
+        in the word_location table for that url and if these two conditions
+        are met - it returns True. Otherwise False.
+        '''
         u = self.con.execute('select rowid from url_list where url = \'%s\''
                 % url).fetchone()
         if u:
@@ -79,11 +103,19 @@ class Crawler(object):
         return False
 
     def add_link_ref(self, url_from, url_to, link_text):
+        '''
+        This function uses the to and from urls to add an entry to the link
+        table. Then it uses the newly created row's rowid, it registers
+        each word of the link text with the link entry in the link table.
+        '''
         words = self.separate_words(link_text)
         from_id = self.get_entry_id('url_list', 'url', url_from)
         to_id = self.get_entry_id('url_list', 'url', url_to)
+        # ensure that the page isnt linking to itself
         if from_id == to_id:
             return
+        # add entry to the link table, which shows the url_ids of the
+        # linking and linked pages
         cur = self.con.execute(
                 'insert into link(from_id, to_id) values (%d, %d)' % \
                         (from_id, to_id))
@@ -92,11 +124,18 @@ class Crawler(object):
             if word in ignore_words:
                 continue
             word_id = self.get_entry_id('word_list', 'word', word)
+            # add entry to the link_words table, which shows what words are
+            # associated with what links.
             self.con.execute(
                     'insert into link_words(link_id, word_id) values (%d, %d)'\
                             % (link_id, word_id))
 
     def crawl(self, pages, depth=2):
+        '''
+        This function, given a list of urls, opens each url and then
+        proceeds to process it and add it to the appropriate tables
+        in the database.
+        '''
         for i in xrange(depth):
             new_pages = set()
             for page in pages:
@@ -106,18 +145,29 @@ class Crawler(object):
                     print "Could not open %s page" % page
                     continue
                 soup = BeautifulSoup(document.read())
+                # process a page and record the findings in the db
                 self.add_to_index(page, soup)
 
+                # scrape all the links from the page
                 links = soup('a')
                 for link in links:
                     if 'href' in dict(link.attrs):
+                        # use urlpar.urljoin to combine user-provided
+                        # absolute url and the anchor address scraped.
                         url = urljoin(page, link['href'])
+                        # continue loop if a single quote mark is in the url
                         if url.find("'") != -1:
                             continue
                         url = url.split("#")[0]
                         if url[0:4] == 'file' or url[0:4] == 'http' and not self.is_indexed(url):
+                            # if the url hasnt been indexed yet, then add it
+                            # to the new_pages set. Using a set ensures that
+                            # there will be no duplicates
                             new_pages.add(url)
                             link_text = self.get_text_only(link)
+                            # use the current page url (page), the linked
+                            # url (url), and the link text to add an entry
+                            # to the link_words table
                             self.add_link_ref(page, url, link_text)
                 self.db_commit()
 
@@ -176,6 +226,13 @@ class Searcher(object):
         self.con.close()
 
     def get_match_rows(self, q):
+        '''
+        This function builds a long sql query that selects the url_id
+        and locations for all the words for all pages (urls) that share
+        the words in the query q.
+        This is later returned as a list of rows, where each row looks
+        something like: [url_id, word 0 location, word 1 location, ... ]
+        '''
         field_list = 'w0.url_id'
         table_list = ''
         clause_list = ''
@@ -190,6 +247,10 @@ class Searcher(object):
             if word_row:
                 word_id = word_row[0]
                 word_ids.append(word_id)
+                # if there is more than 1 word in the query, we will break
+                # the table list apart with a comma and the clause list
+                # with an AND. We then add the clause that both queries
+                # should affect the same url (url_id = url_id)
                 if table_number > 0:
                     table_list += ','
                     clause_list += ' and '
@@ -202,12 +263,21 @@ class Searcher(object):
 
         full_query = 'select %s from %s where %s' % \
                 (field_list, table_list, clause_list)
+        # execute the built query, which will return url_ids paired with
+        # a words location under that url for only the urls, which contain
+        # all of the specified words.
         cur = self.con.execute(full_query)
         rows = [row for row in cur]
 
         return rows, word_ids
 
     def get_scored_list(self, rows, word_ids):
+        '''
+        Applies the list of weights to the results of get_match_rows.
+        Returns a list of urls with each normalized score added to
+        each url.
+        '''
+
         total_scores = dict([(row[0], 0) for row in rows])
 
         #weights = [(1.0, self.frequency_score(rows))]
@@ -227,10 +297,18 @@ class Searcher(object):
         return total_scores
 
     def get_url_name(self, id):
+        '''
+        Retrieves the url given a url_list rowid.
+        '''
         return self.con.execute(
                 'select url from url_list where rowid=%d' % id).fetchone()[0]
 
     def query(self, q):
+        '''
+        First it finds the relevent rows of urls that match the query q,
+        then it applies scoring and weighing to those urls, and finally
+        returns a sorted list with string urls for readability.
+        '''
         rows, word_ids = self.get_match_rows(q)
         scores = self.get_scored_list(rows, word_ids)
         ranked_scores = sorted([(score, url)
@@ -240,13 +318,11 @@ class Searcher(object):
 
         return word_ids, [r[1] for r in ranked_scores[:10]]
 
-    def nn_score(self, rows, word_ids):
-        url_ids = [url_id for url_id in set([row[0] for row in rows])]
-        nn_result = mynet.get_result(word_ids, url_ids)
-        scores = dict([(url_ids[i], nn_result[i]) for i in xrange(len(url_ids))])
-        return self.normalize_scores(scores)
-
     def normalize_scores(self, scores, small_is_better=False):
+        '''
+        It divides each score by the total number of scores for this category,
+        so that different scoring methods all supply a unified score.
+        '''
         vsmall = 0.00001
         if small_is_better:
             min_score = min(scores.values())
@@ -323,5 +399,11 @@ class Searcher(object):
                 for (u, l) in link_scores.items()])
             return normalized_scores
 
+    # neural network scoring
 
+    def nn_score(self, rows, word_ids):
+        url_ids = [url_id for url_id in set([row[0] for row in rows])]
+        nn_result = mynet.get_result(word_ids, url_ids)
+        scores = dict([(url_ids[i], nn_result[i]) for i in xrange(len(url_ids))])
+        return self.normalize_scores(scores)
 
